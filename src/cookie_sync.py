b7b02fc2acf0a10136123eb63699df26a8e4d3cf
pavasgdb <45535940+pavasgdb@users.noreply.github.com>
2021-11-13 15:48:45 +0530

    Update cookie_sync.py

--- src/cookie_sync.py
+++ src/cookie_sync.py
@@ -3,12 +3,6 @@ import json
 from urllib.parse import urlparse
 import requests
 
-url = "https://zozor54-whois-lookup-v1.p.rapidapi.com/"
-
-headers = {
-    'x-rapidapi-host': "zozor54-whois-lookup-v1.p.rapidapi.com",
-    'x-rapidapi-key': "9f1a548bd3msh9d854107242355ap1736efjsne3e7110bee95"
-    }
 
 with open(sys.argv[1], encoding='utf-8') as f:
     
